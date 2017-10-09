c48e009c73b685d2050da21341e873d89ffe6fed
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2017-10-09 23:57:28 +0800

    force push

---
+++ CprimePlus/102/cmake-build-debug/CMakeFiles/schoolexp201.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/schoolexp201.dir/school-exp002-1.c.obj"
+  "schoolexp201.pdb"
+  "schoolexp201.exe"
+  "schoolexp201.exe.manifest"
+  "libschoolexp201.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/schoolexp201.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
