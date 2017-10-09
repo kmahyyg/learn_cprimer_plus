18e9e45b6d2abff638caf62fcdf25296e7a167eb
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2017-10-09 20:05:30 +0800

    Auto Commit&Push : Add learning files
    Signed-off-by: Patrick Young <16604643+kmahyyg@users.noreply.github.com>

--- CprimePlus/102/school-exp002.c
+++ CprimePlus/102/school-exp002.c
@@ -1,15 +1,16 @@
 // Filename : school-exp002.c
 // Created by Yyg on 10/9/2017.
-// Description : Exp 2
+// Description : Exp 2 , Standard C99
 
 #include <stdio.h>
 
 int main(void) {
     printf("001. %d \n", 3 + 4 * 6);
     printf("002. %d \n", 3 * 4 / 6 + 6);
-    printf("003. %d \n", 2 * 3 / 12 * 8 / 4);
+    printf("003. %d \n", 2 * 3 / 12 * 8 / 4);       /* Computer always treat it as pre-defined int and calculate in order
+ so 2 * 3 = 6 , 6 / 12 = 0 , 0 * 8 / 4 = 0 , so the result is 0 instead of 1 */
     printf("004. %d \n", 10 * (1 + 7 * 3));
-    printf("005. %d \n", 20 - 2 / 6 + 3);
+    printf("005. %d \n", 20 - 2 / 6 + 3);        /* The same treatment as line 10 Because of the same reason */
     printf("006. %d \n", 20 - 2 / (6 + 3));
     printf("007. %d \n", (20 - 2) / 6 + 3);
     printf("008. %d \n", (20 - 2) / (6 + 3));
@@ -23,7 +24,16 @@ int main(void) {
     printf("015. %8.2f \n", (20.0 - 2.0) / 6.0 + 3.0);
     printf("016. %8.2f \n", (20.0 - 2.0) / (6.0 + 3.0));
     printf("----------------------------- \n");
-
+// ASCII CODE: m - 109 , G -  71 , b - 98 , a - 97 , g - 103 , A - 65
+    printf("017. %c \n", 'm' - 5);
+    printf("018. %c \n", 'm' + 5);
+    printf("019. %c \n", 'G' + 6);
+    printf("020. %c \n", 'G' - 6);
+    printf("----------------------------- \n");
+// If allowed , I'll try to use the short int(Dec) instead of int(Dec)
+    printf("021. %d \n", 'b' - 'a');
+    printf("022. %d \n", 'g' - 'a' + 1);
+    printf("023. %d \n", 'G' - 'A' + 1);
     getchar();
 }
 
