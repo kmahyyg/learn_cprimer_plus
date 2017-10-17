4f3397833228d1d39dd8f6f9bf3fd6a6262f06c2
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2017-10-17 19:15:26 +0800

    Add School Experience 4 : Part one : sort 3 numbers
    Signed-off-by: Patrick Young <16604643+kmahyyg@users.noreply.github.com>

--- CprimePlus/102/school-exp004.c
+++ CprimePlus/102/school-exp004.c
@@ -1,8 +1,28 @@
+// Filename : school-exo004.c
+// Created by Yyg on 10/17/2017
+// Description : Compare between three numbers and output in order
+// Something like bubble sort
+
 #include <stdio.h>
 
 int main(void){
-    int a,b,c ;
-    printf("Please input 3 numbers: ");
-    scanf("%d %d %d", &a &b &c);
-    
+    double a, b, c, tempvar;
+    scanf("%lf %lf %lf", &a, &b, &c);
+    if (a > b) {
+        tempvar = b;
+        b = a;
+        a = tempvar;
+    }
+    if (a > c) {
+        tempvar = c;
+        c = a;
+        a = tempvar;
+    }
+    if (b > c) {
+        tempvar = c;
+        c = b;
+        b = tempvar;
+    }
+    printf("%.2lf %.2lf %.2lf \n", a, b, c);
+    return 0;
 }
