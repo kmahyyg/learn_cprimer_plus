d8f34a36e1d3a835ebf664601564cc65c5b04483
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2017-10-17 22:41:51 +0800

    Experience 4 Optimization : User-friendly output and input
    Signed-off-by: Patrick Young <16604643+kmahyyg@users.noreply.github.com>

--- CprimePlus/102/school-exp004.c
+++ CprimePlus/102/school-exp004.c
@@ -1,12 +1,13 @@
 // Filename : school-exo004.c
 // Created by Yyg on 10/17/2017
 // Description : Compare between three numbers and output in order
-// Something like bubble sort
+
 
 #include <stdio.h>
 
 int main(void){
-    double a, b, c, tempvar;
+    double a, b, c, tempvar;    // define a tempvar to make the var cross-equal
+    printf("Please input 3 numbers to sort:");
     scanf("%lf %lf %lf", &a, &b, &c);
     if (a > b) {
         tempvar = b;
@@ -23,6 +24,10 @@ int main(void){
         c = b;
         b = tempvar;
     }
+    printf("After sorting all 3 numbers , 3 numbers from the biggest to the smallest is :");
     printf("%.2lf %.2lf %.2lf \n", a, b, c);
+    printf("The biggest one is : %.2lf \n", a);
+    printf("The smallest one is : %.2lf \n", c);
+    getchar();
     return 0;
 }
