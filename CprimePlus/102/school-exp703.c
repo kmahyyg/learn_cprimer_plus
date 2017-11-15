38e4f06497389f55b3b270acf56624608dd0ad16
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2017-11-15 20:30:02 +0800

    Add the following files:
    (1) School Experience 7, Question 3 of Selection Sort algorithm.
    
    Signed-off-by: Patrick Young <16604643+kmahyyg@users.noreply.github.com>

---
+++ CprimePlus/102/school-exp703.c
@@ -0,0 +1,78 @@
+/*
+ *     Copyright (C) <2017>  <Patrick Young (kmahyyg@Github)>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+   *
+   */
+
+// Filename: school-exp703.c
+// Created by Yyg on 11/15/2017.
+// Desc: sort algorithm using bubble sort and selection sort
+// REF: https://zh.wikipedia.org/wiki/%E5%86%92%E6%B3%A1%E6%8E%92%E5%BA%8F
+// REF: https://zh.wikipedia.org/wiki/%E9%80%89%E6%8B%A9%E6%8E%92%E5%BA%8F
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// Stdlib.h used to generate 10 pseudo int number
+
+void array_selection_sort(int *array1);
+
+int array_bubble_sort(int *array2);
+
+void swap(int *num_a, int *num_b) {
+    int temp = *num_b;
+    *num_b = *num_a;
+    *num_a = temp;
+}
+
+int main(void) {
+    int testarr[10];
+    for (int i = 0; i < 10; i++) {
+        testarr[i] = rand();
+    }
+    int decision;
+    printf("Choose a sort: \n 1.Selection Sort \n 2.Bubble Sort ");
+    scanf("%d", &decision);
+    if (decision == 1) {
+        array_selection_sort(testarr);
+    } else if (decision == 2) {
+        array_bubble_sort(testarr);
+    } else if (decision != 1 && decision != 2) {
+        printf("Illegal input!");
+        scanf("%d", &decision);
+    }
+    return 0;
+}
+
+void array_selection_sort(int *array1) {
+//    printf("test func of selection");
+    for (int i = 0, min = 0; i < 9; i++) {
+        min = i;
+        for (int j = i + 1; j < 10; j++) {
+            if (array1[min] < array1[j]) {
+                min = j;
+            }
+            swap(&array1[min], &array1[j]);
+        }
+    }
+    for (int k = 0; k < 10; k++) {
+        printf("[%d] is %d \n", k + 1, array1[k]);
+    }
+}
+
+int array_bubble_sort(int *array2) {
+//    printf("test func of selection");
+
+}
