ee8e46305b30318db2ee84347549fdcf56028a86
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2017-11-16 19:33:46 +0800

    [debug] schoolexp701: fact() data overflow.
    Signed-off-by: Patrick Young <16604643+kmahyyg@users.noreply.github.com>

--- CprimePlus/102/school-exp701.c
+++ CprimePlus/102/school-exp701.c
@@ -16,27 +16,30 @@
    *
    */
 
-// Filename : school-exp601.c
-// Created by Yyg on 11/14/2017.
-// Desc: Array EXP: print 10 lines of YangHui triangle
+/*  Filename : school-exp601.c
+ Created by Yyg on 11/14/2017.
+ Desc: Array EXP: print 10 lines of YangHui triangle
+ long long data type usage notice:
+https://stackoverflow.com/questions/2844/how-do-you-printf-an-unsigned-long-long-intthe-format-specifier-for-unsigned-lo
+https://sourceforge.net/p/mingw-w64/mailman/mingw-w64-public/thread/20120411101049.GA4263@glandium.org/
+https://stackoverflow.com/questions/462345/format-specifier-for-long-long  */
 
 #include <stdio.h>
 
-long fact(int N);              // lead the factorial(N) in to help calculate the combination
-long combination(int K, int R);
+long long fact(int N);              // lead the factorial(N) in to help calculate the combination
+long long combination(int K, int R);
 
-
-long fact(int N) {
+long long fact(int N) {
     int i;
-    long result = 1;
+    long long result = 1;
     for (i = 1; i <= N; i++) {
         result *= i;
     }
     return result;
 }
 
-long combination(int K, int R) {               //C(k,r)= r! / ((r-k)! * k!)
-    long combined;
+long long combination(int K, int R) {               //C(k,r)= r! / ((r-k)! * k!)
+    long long combined;
     combined = (fact(R)) / (fact(R - K) * fact(K));
     return combined;
 }
@@ -47,9 +50,10 @@ int main(void) {
     scanf("%d", &Longest);                                                // Longest must < 13
     for (Linenum = 0; Linenum <= Longest; Linenum++) {
         for (int j = 0; j <= Linenum; j++) {
-            long combined;
+            long long combined;
             combined = combination(j, Linenum);
-            printf(" %ld ", combined);
+            printf(" %I64u ", combined);
+            // longlong should use %lld in Linux , but mscvrt doesn't support them , use %I64u instead
         }
         printf("\n");
     }
