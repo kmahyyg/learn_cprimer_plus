36239bbbed65110c274a058354257eed3230eea9
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2017-11-17 01:01:16 +0800

    [debug] rewrite: school-exp701.c
    First step : rewrite a lot of mods.
    
    Signed-off-by: Patrick Young <16604643+kmahyyg@users.noreply.github.com>

--- CprimePlus/102/school-exp704.c
+++ CprimePlus/102/school-exp704.c
@@ -15,22 +15,33 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
    *
    */
-
-// Filename: school-exp704.c
-// Created by Yyg on 11/16/2017.
-// Desc: binary search with array
-
+// #define GET_ARRAY_LEN(array,len){len = (sizeof(array) / sizeof(array[0]));}
+// Filename : school-exp704.c
+// Created by Yyg on 11/17/2017.
+// Desc : Use binary search
 /* the binary search is a divided search , this search need an ordered sequence
  * this algorithm will cost log2(n+1) times and the difficulties are O(log2n)
- * this sequence must not be a chain table.DONNOT try to use this algorithm apply into the frequently data-inserted seq.
+ * this sequence must not be a chain table. DON'T try to use this algorithm apply into the frequently data-inserted seq.
  */
 
-//#define GET_ARRAY_LEN(array,len){len = (sizeof(array) / sizeof(array[0]));}
 #include <stdio.h>
 
+void swap(int *num_a, int *num_b);
+
+int *generate_array(int N);
 int *array_selection_sort(int *array1, int N);
 
-int bin_search(int seanum, int *arra1, int arrlen);
+int main(void) {
+    // Need an input of predefined array length and get data also predefined a new array //
+    int N;
+    printf("input the array length:");
+    scanf("%d", &N);
+    int *array101 = generate_array(N);
+    // Need an input of predefined array length and get data also predefined a new array//
+    // Now , use array101 to input to another func to sort. Prepare for binary search //
+    int *array102 = array_selection_sort(array101, N);
+    return 0;
+}
 
 void swap(int *num_a, int *num_b) {
     int temp = *num_b;
@@ -38,39 +49,21 @@ void swap(int *num_a, int *num_b) {
     *num_a = temp;
 }         // Item in array swap succuessful example
 
-
-int main(void) {
-    // decide the array length
-    printf("Input the array length:");
-    int N;
-    scanf("%d", &N);         //use stdin redirect with stdout
-    int testarr[N];
-    printf("testarr is an array.");
-    printf("this array length is %d . \n", N);
-    for (int i = 0; i < N; i++) {
-        int input2;
-        printf("please input each item: \n");
-        scanf("%d", &input2);
-        testarr[i] = input2;
+int *generate_array(int N) {
+    int *array1[N];
+    for (int i = 0; i < (N - 1); i++) {
+        int input_data;
+        printf("Please input data, one data each time: \n");
+        scanf("%d", &input_data);
+        array1[i] = &input_data;
     }
-    // generate a pseudo random array
-    int *array2_sorted[N];
-    *array2_sorted = array_selection_sort(testarr, N);
-    // copied to another array for next step
-    int search_num;
-    printf("input the num you want to search for:");
-    scanf("%d", &search_num);
-    int result;
-    result = bin_search(search_num, array2_sorted, N);
-    printf("the index is %d ", result);
-    return 0;
+    return *array1;
 }
 
-int *array_selection_sort(int *array1, int N) {
-//    printf("test func of selection");
-    for (int i = 0, min = 0; i < N; i++) {
+int *array_selection_sort(int *array1, int N) {      // N is the predefined array length
+    for (int i = 0, min = 0; i < (N - 1); i++) {
         min = i;
-        for (int j = i + 1; j < (N + 1); j++) {
+        for (int j = i + 1; j < N; j++) {
             if (array1[min] < array1[j]) {
                 min = j;
             }
@@ -80,28 +73,6 @@ int *array_selection_sort(int *array1, int N) {
     for (int k = 0; k < N; k++) {
         printf("[%d] is %d \n", k + 1, array1[k]);
     }
-    return array1;
+    return array1;   //Selection Sort
 }
-
-int bin_search(int seanum, int *arra1, int arrlen) {
-    int indEx, i = 0;
-    int midnum;
-    while (i <= arrlen) {
-        midnum = (arrlen - 1) / 2;
-        if (seanum < arra1[0] && seanum > arra1[arrlen]) {
-            printf("The num you search is not existed in this sequence");
-            indEx = -1;
-        } else if (seanum == arra1[midnum]) {
-            i = midnum;
-            indEx = i;
-        } else if (seanum < arra1[midnum]) {
-            i = midnum - 1;
-        } else if (seanum > arra1[midnum]) {
-            i = midnum + 1;
-        }
-    }
-    indEx = i;
-    return indEx;
-}
-
 
