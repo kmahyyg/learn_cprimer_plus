92da49f223b4eced7563b2ac630514b12eda2743
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2017-11-19 01:14:52 +0800

    [optimize] geekunion3.c : correct identifier for MinGW-w64
    size_t required an unsigned 64bit int.
    %I64d is required instead of %lld/%d
    
    All info provided by Clion IDE.
    
    Signed-off-by: Patrick Young <16604643+kmahyyg@users.noreply.github.com>

--- CprimePlus/102/geekunion3.c
+++ CprimePlus/102/geekunion3.c
@@ -5,15 +5,15 @@
 #include <stdio.h>
 
 int main(void){
-    printf("The length of int i3s : %d bytes. \n", sizeof(int));
-    printf("The length of char i4s : %d bytes. \n", sizeof(char));
-    printf("The length of double i5s : %d bytes. \n", sizeof(double));
-    printf("The length of long i4s : %d bytes. \n", sizeof(long));
-    printf("The length of short i5s : %d bytes. \n", sizeof(short));
-    printf("The length of float i5s : %d bytes. \n", sizeof(float));
-    printf("The length of longlong i9s : %d bytes. \n", sizeof(long long));
-    printf("The length of longdouble i11s : %d bytes. \n", sizeof(long double));
-    printf("The length of short int i9s : %d bytes. \n", sizeof(short int));
+    printf("The length of int i3s : %I64d bytes. \n", sizeof(int));
+    printf("The length of char i4s : %I64d bytes. \n", sizeof(char));
+    printf("The length of double i5s : %I64d bytes. \n", sizeof(double));
+    printf("The length of long i4s : %I64d bytes. \n", sizeof(long));
+    printf("The length of short i5s : %I64d bytes. \n", sizeof(short));
+    printf("The length of float i5s : %I64d bytes. \n", sizeof(float));
+    printf("The length of longlong i9s : %I64d bytes. \n", sizeof(long long));
+    printf("The length of longdouble i11s : %I64d bytes. \n", sizeof(long double));
+    printf("The length of short int i9s : %I64d bytes. \n", sizeof(short int));
     return 0;
 }
 
