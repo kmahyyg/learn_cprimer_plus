e63c713a2faad18b082b22dd8f3d9dda06c22295
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2017-11-22 23:52:58 +0800

    [fake] finish : school-exp803.c
    The optional part of the School Experience 8 , This question is going to use the dictionary-like data type, which I couldn't handle now. I thought that maybe one day I could use malloc() or map() to solve that, because I just need a hash table instead of a f*cking variable-length array.
    
    I will write a Python 3 version later.
    
    Signed-off-by: Patrick Young <16604643+kmahyyg@users.noreply.github.com>

---
+++ CprimePlus/102/school-exp803.c
@@ -0,0 +1,27 @@
+/*
+ *     Copyright (C) <2017>  <Patrick Young (kmahyyg@Github)>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+   *
+   */
+
+// Filename: school-exp803.c
+// Created by Yyg on 11/22/2017.
+/* Desc: Input multi-line strings,
+ * calculate the string length,
+ * upper-ordered data length,
+ * print the line graph */
+
+/* TODO :  :( Unknown how to do */
+/* See Python version here: kmahyyg/learn_py3.git @ Github */
