a1d6ecab9a78a13f7fffc5526482df78cd122a7d
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2017-11-29 23:11:05 +0800

    [debug succeeded] school_exp901.c

--- CPrimePlus 2/103/school_exp901.c
+++ CPrimePlus 2/103/school_exp901.c
@@ -2,13 +2,16 @@
 // Created by Yyg on 11/29/2017.
 // Desc: 5 stuno + name + 3 grade + avg
 
+#define MAXLEN 1025
+typedef char String[MAXLEN];
+
 #include <stdio.h>
 #include <stdlib.h>
 
 double get_and_out(char *str1){
     double rev;
     gets(str1);
-    rev=atof(str1);
+    rev=atof(str1);     // strtod() more safely
     return rev;
 }
 
@@ -19,18 +22,20 @@ void wrt_format(char *str,FILE *fp){
 
 int main(void){
     FILE *ftp = NULL;
-    int status =1;
-    char *sheetheader, *spliter,*name="",*studentid="",*grade1="",*grade2="",*grade3="",*avggra="";
+    int status =0;
+    String name,studentid,grade1,grade2,grade3,avggra;
     double grade1_f,grade2_f,grade3_f,averagegrade_f,sum1=0.00;
-    sheetheader = "|:Student ID No:|:Name:|:Grade 1:|:Grade 2:|:Grade 3:|:Average Grade:|";
-    spliter = "|---------------|------|---------|---------|---------|---------------|";
+    char *sheetheader = "|Student ID No|Name|Grade 1|Grade 2|Grade 3|Average Grade|";
+    char *spliter =  "|:-----------:|:--:|:-----:|:-----:|:-----:|:-----------:|";
     ftp = fopen("stud.txt","w+");
     fputs(sheetheader,ftp);
+    fputs("\n",ftp);
     fputs(spliter,ftp);
     fputs("\n",ftp);
-    while (status == 1){
+    for (status = 0; status < 3; status++){
         printf("Input the student number:");
-        gets(studentid);
+        gets(studentid);      // char *studentid; fgets(studentid,10,stdin);
+        fputs("|",ftp);
         wrt_format(studentid,ftp);
         printf("Input the name:");
         gets(name);
@@ -48,11 +53,9 @@ int main(void){
         wrt_format(grade3,ftp);
         sum1 += grade3_f;
         averagegrade_f = sum1 / 3.00;
-        avggra = gcvt(averagegrade_f,5,avggra);
+        gcvt(averagegrade_f,5,avggra);
         fputs(avggra,ftp);
-        fputs("|\n",ftp);
-        printf("Continue? 1.Y 2.N");
-        status = getchar();
+        fputs("| \n",ftp);
     }
     return 0;
 }
