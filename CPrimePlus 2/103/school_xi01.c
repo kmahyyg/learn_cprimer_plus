08d4b2a5ac9dd5d153cc514af31cbb05fd2a6c1f
Patrick Young <16604643+kmahyyg@users.noreply.github.com>
2017-12-18 23:55:20 +0800

    Finish XI EXP Q1

--- CPrimePlus 2/103/school_xi01.c
+++ CPrimePlus 2/103/school_xi01.c
@@ -3,7 +3,6 @@
 // Desc: Struct save student data
 
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 
 #define MAXLENGTH 1024
@@ -12,25 +11,33 @@ typedef char String[MAXLENGTH + 1];
 
 struct Studentdata {
     String sid, name, grade1, grade2, grade3;
-    double gr1, gr2, gr3, avggrade;
+    float gr1, gr2, gr3;
+    double avggrade;
 };
 
 
 void getstudata(struct Studentdata *stu1) {
+    printf("Input your id:");
     gets(stu1->sid);
+    printf("Input your name:");
     gets(stu1->name);
+    printf("3 Grades, one by one.");
     gets(stu1->grade1);
     gets(stu1->grade2);
     gets(stu1->grade3);
+    stu1->gr1 = strtof(stu1->grade1, NULL);
+    stu1->gr2 = strtof(stu1->grade2, NULL);
+    stu1->gr3 = strtof(stu1->grade3, NULL);
+    stu1->avggrade = ((stu1->gr1) + (stu1->gr2) + (stu1->gr3)) / 3.00;
 }
 
 void prtstudata(struct Studentdata *stu1) {
     printf("Student ID: %s \n", stu1->sid);
-    printf("Student Name: %s", stu1->name);
-    printf("Grade1: %s", stu1->grade1);
-    printf("Grade2: %s", stu1->grade2);
-    printf("Grade3: %s", stu1->grade3);
-    printf("Student Average Grade: %lf \n", stu1->avggrade);
+    printf("Student Name: %s \n", stu1->name);
+    printf("Grade1: %.2f \n", stu1->gr1);
+    printf("Grade2: %.2f \n", stu1->gr2);
+    printf("Grade3: %.2f \n", stu1->gr3);
+    printf("Student Average Grade: %.2lf \n", stu1->avggrade);
 }
 
 int main(void) {
