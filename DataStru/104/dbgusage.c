0c1aa249220b2bd6d0ee9e5a83350d142fac6bed
kmahyyg <16604643+kmahyyg@users.noreply.github.com>
2018-09-23 11:51:06 +0800

    move data structure files to another repo

--- DataStru/104/dbgusage.c
+++
@@ -1,4 +0,0 @@
-// Filename: dbgusage.c
-// Created by kmahyyg on 9/18/18.
-// Purpose: For fast debug some function.
-
--- DataStru/104/exp1.c
+++
@@ -1,5 +0,0 @@
-// Filename: exp1.c
-// Created by kmahyyg on 9/18/18.
-// Purpose: Complex Number calculator (+-*/)
-// Up to Score: 90, #TODO
-
